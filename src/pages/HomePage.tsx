dc8c7ff926b7e51d9b168ce03d2abca530a58b28
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-10 15:16:25 +0530

    [Sparsh] Add: Navbar, Footer Added to HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+function HomePage() {
+    return (
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    )
+}
+
+export default HomePage
