8dada18b3e07d1c4b4853b8477d404a86c617d28
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-10 15:08:50 +0530

    [Sparsh] Add: Responsive Footer Component Created

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div className='h-[50px] w-[100%] bg-[#2E1D1E] flex justify-center'>
+            <div className='text-[12px] w-[80%] text-white flex items-center max-md:w-[90%]'>
+                <p>Copyright © 2020, Bookstore Private Limited. All Rights Reserved</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
