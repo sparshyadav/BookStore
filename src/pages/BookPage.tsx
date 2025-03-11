81aecc02dbfd1eba4ac8772b04ffdc3b8dfa9437
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-11 18:47:54 +0530

    [Sparsh] Add: Individual Book Responsive Page Created

---
+++ src/pages/BookPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import BookPageContainer from '../components/BookPageContainer'
+import Footer from '../components/Footer'
+
+function BookPage() {
+    return (
+        <>
+            <Navbar />
+            <BookPageContainer />
+            <Footer />
+        </>
+    )
+}
+
+export default BookPage
