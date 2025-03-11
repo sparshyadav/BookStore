30db5941df12f40d7c27e5d8313e828543166f3c
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-11 18:55:34 +0530

    [Sparsh] Refactor: Mobile Responsiveness Implemented for HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import BooksContainer from '../components/BooksContainer'
 
 function HomePage() {
     return (
         <>
             <Navbar />
+            <BooksContainer />
             <Footer />
         </>
     )
