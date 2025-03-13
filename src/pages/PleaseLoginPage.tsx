b4dd5f3ceee684271ae6ab9d71c8572a6f8e88ce
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-13 12:06:49 +0530

    [Sparsh] Refactor: Please Login Responsive Page Created

---
+++ src/pages/PleaseLoginPage.tsx
@@ -0,0 +1,16 @@
+import pleaseLoginImage from '../assets/please-login-image.svg';
+
+function PleaseLoginPage() {
+    return (
+        <div className="flex justify-center items-center h-[100vh] w-[100%]">
+            <div className="w-[275px] flex flex-col items-center justify-center">
+                <p className="text-[25px]">Please Login</p>
+                <p className="text-[#9D9D9D] text-[15px]">Login to view items in your wishlist</p>
+                <img className="h-[75px] w-[65px] !my-[25px]" src={pleaseLoginImage} />
+                <button className='border border-[#A03037] w-[130px] h-[35px] text-[#A03037] text-[12px] cursor-pointer hover:bg-[#A03037] hover:text-white transition-all duration-300'>Login/Signup</button>
+            </div>
+        </div>
+    )
+}
+
+export default PleaseLoginPage
