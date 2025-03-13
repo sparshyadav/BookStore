ba9103998c3d1f690f3397e5d5a371227e068ccb
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-13 18:55:46 +0530

    [Sparsh] Add: My Order Page Created

---
+++ src/pages/MyOrderPage.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import MyOrderContainer from '../components/MyOrderContainer'
+
+function WishlistPage() {
+    return (
+        <>
+            <Navbar />
+            <MyOrderContainer />
+            <Footer />
+        </>
+    )
+}
+
+export default WishlistPage
