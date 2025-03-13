b5c45fe38b5543f389b3432843b9f32fdafbd2f9
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-13 12:05:48 +0530

    [Sparsh] Refactor: Order Confirm Responsive Page Created

---
+++ src/pages/OrderConfirmPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import OrderConfirmContainer from '../components/OrderConfirmContainer'
+import Footer from '../components/Footer'
+
+function OrderConfirmPage() {
+    return (
+        <>
+            <Navbar />
+            <OrderConfirmContainer />
+            <Footer />
+        </>
+    )
+}
+
+export default OrderConfirmPage
