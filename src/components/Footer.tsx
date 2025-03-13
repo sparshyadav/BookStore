c2b2d79dcefdc319e207b8ef84f5d06330964d39
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-13 12:12:39 +0530

    [Sparsh] Refactor: Footer Context Width Adjusted

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <div className='h-[50px] w-[100%] bg-[#2E1D1E] flex justify-center'>
-            <div className='text-[12px] w-[80%] text-white flex items-center max-md:w-[90%]'>
+            <div className='text-[12px] w-[67%] text-white flex items-center max-md:w-[90%]'>
                 <p>Copyright © 2020, Bookstore Private Limited. All Rights Reserved</p>
             </div>
         </div>
