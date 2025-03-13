360a34c8b0be02f1a33063ddb46a7bb93de758aa
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-13 12:06:11 +0530

    [Sparsh] Refactor: Wishlist Responsive Page Created

---
+++ src/pages/WishlistPage.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import WishlistContainer from '../components/WishlistContainer'
+
+function WishlistPage() {
+    return (
+        <>
+            <Navbar />
+            <WishlistContainer />
+            <Footer />
+        </>
+    )
+}
+
+export default WishlistPage
