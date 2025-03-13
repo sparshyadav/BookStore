c54c5877852491f5a8ce50429577f213c01486ac
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-13 12:08:23 +0530

    [Sparsh] Refactor: Unnecessary Imports Removed

--- src/pages/OrderConfirmPage.tsx
+++ src/pages/OrderConfirmPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
 import OrderConfirmContainer from '../components/OrderConfirmContainer'
 import Footer from '../components/Footer'
