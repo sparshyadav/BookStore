a372e204202ec9ced322aed4042de369d88df471
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-13 12:07:50 +0530

    [Sparsh] Add: My Cart Page Cart Component Rendered in My Cart Page

---
+++ src/pages/MyCartPage.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import MyCartContainer from '../components/MyCartContainer'
+
+function MyCartPage() {
+    return (
+        <div>
+            <Navbar />
+            <MyCartContainer />
+            <Footer />
+        </div>
+    )
+}
+
+export default MyCartPage
