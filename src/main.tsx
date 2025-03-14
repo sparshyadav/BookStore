afab2cf539b37ab2a46dbd0c74b8c036ab2bfdba
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-14 11:17:29 +0530

    [Sparsh] Refactor: App Component Wrapped in Redux Provider for Global State

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+      <App />
+      <ToastContainer position="top-right" autoClose={5000} />
+    </Provider>
+  </StrictMode >,
 )
