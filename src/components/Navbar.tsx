f9a005c0795d03927f07f6aad8f56995056a34e3
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-14 11:18:44 +0530

    [Sparsh] Refactor: Dynamic Profile Page With Logout Functionality Implemented in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import educationImage from '../assets/education.svg'
-import { User, ShoppingCart, Search } from 'lucide-react';
+import educationImage from '../assets/education.svg';
+import { User, ShoppingCart, Search, Wallet, Heart } from 'lucide-react';
 
 function Navbar() {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
     return (
-        <div className='h-[60px] w-[100%] bg-[#A03037] flex justify-center fixed z-100'>
-            <div className='h-[100%] w-[70%] max-w-7xl flex justify-between max-sm:w-[100%] max-lg:w-[90%]'>
+        <div className='h-[60px] w-[100%] bg-[#A03037] flex justify-center fixed z-50 overflow-visible'>
+            <div className='h-[100%] w-[70%] max-w-7xl flex justify-between max-sm:w-[100%] max-lg:w-[90%] relative'>
                 <div className='h-[100%] w-[200px] flex items-center justify-center gap-5 max-sm:gap-2'>
                     <NavLink to={'/orderconfirm'}>
-                        <img src={educationImage} alt='Image Not Found' className='max-sm:h-[15px]' /></NavLink>
+                        <img src={educationImage} alt='Image Not Found' className='max-sm:h-[15px]' />
+                    </NavLink>
                     <p className='text-white text-[25px] max-sm:text-[18px]'>BookStore</p>
                 </div>
+
                 <div className='h-[100%] w-[80%] flex max-md:justify-end'>
                     <div className='h-[100%] w-[75%] flex items-center max-md:hidden'>
                         <div className="w-[75%] h-[40px] flex items-center gap-2 rounded-[3px] bg-white border pl-[10px]">
@@ -22,13 +27,69 @@ function Navbar() {
                             />
                         </div>
                     </div>
-                    <div className='h-[100%] w-[25%] flex max-md:justify-end max-md:w-[500px] max-sm:w-[300px]'>
+                    <div className='h-[100%] w-[25%] flex relative max-md:justify-end max-md:w-[500px] max-sm:w-[300px]'>
                         <div className='bg-[#A03037] h-[100%] w-[80px] text-white hidden max-md:flex justify-center items-center max-md:w-[75px] max-sm:w-[50px]'>
                             <Search className="w-[25px] h-[25px] max-md:w-[22px] max-md:h-[22px] max-sm:w-[18px] max-sm:h-[18px]" />
                         </div>
-                        <div className='bg-[#A03037] h-[100%] w-[80px] flex flex-col justify-center items-center gap-1 text-white max-md:w-[75px] max-sm:w-[50px]'>
-                            <User className="w-[25px] h-[25px] max-md:w-[22px] max-md:h-[22px] max-sm:w-[18px] max-sm:h-[18px]" />
-                            <p className="text-[10px] hidden md:block">Profile</p>
+                        <div className='relative'>
+                            <div
+                                className='bg-[#A03037] h-[100%] w-[80px] flex flex-col justify-center items-center gap-1 text-white max-md:w-[75px] max-sm:w-[50px] cursor-pointer'
+                                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                            >
+                                <User className="w-[25px] h-[25px] max-md:w-[22px] max-md:h-[22px] max-sm:w-[18px] max-sm:h-[18px]" />
+                                <p className="text-[10px] hidden md:block">Profile</p>
+                            </div>
+                            {
+                                isDropdownOpen && (
+                                    <div className="fixed top-[60px] left-[65%] !ml-[10px] max-w-[260px] !pr-[50px] !pl-[15px] !py-[15px] bg-white shadow-lg text-black flex flex-col z-[999] max-[768px]:left-[50%] max-[640px]:left-[45%] max-[450px]:left-[25%]">
+                                        {
+                                            (() => {
+                                                const tokenData = JSON.parse(localStorage.getItem("token") || "{}");
+                                                return tokenData.name ? (
+                                                    <>
+                                                        <p className="text-[#0A0102] text-[18px] !mb-[5px]">Welcome, {tokenData.name}</p>
+                                                        <div className='flex gap-1 text-[#878787] items-center !mb-[5px] hover:bg-gray-100 cursor-pointer'>
+                                                            <User className='h-[15px] w-[15px]' />
+                                                            <NavLink to="/wishlist" className="px-3 py-2 text-[15px]">Profile</NavLink>
+                                                        </div>
+                                                        <div className='flex gap-1 text-[#878787] items-center !mb-[5px] hover:bg-gray-100 cursor-pointer'>
+                                                            <Wallet className='h-[15px] w-[15px]' />
+                                                            <NavLink to="/wishlist" className="px-3 py-2 text-[15px]">My Wishlist</NavLink>
+                                                        </div>
+                                                        <div className='flex gap-1 text-[#878787] items-center !mb-[5px] hover:bg-gray-100 cursor-pointer'>
+                                                            <Heart className='h-[15px] w-[15px]' />
+                                                            <NavLink to="/myorder" className="px-3 py-2 text-[15px]">My Orders</NavLink>
+                                                        </div>
+                                                        <NavLink to={'/'}>
+                                                            <button onClick={()=>localStorage.removeItem("token")} className='w-[125px] h-[30px] text-[#A03037] border border-[#A03037] !mt-[10px] hover:cursor-pointer hover:bg-[#B03A42] hover:text-white duration-300'>
+                                                                Logout
+                                                            </button>
+                                                        </NavLink>
+                                                    </>
+                                                ) : <>
+                                                    <p className="text-[#0A0102] text-[18px]">Welcome, {tokenData.name}</p>
+                                                    <p className="text-[#878787] text-[12px]">To access account and manage orders</p>
+                                                    <NavLink to={'/'}>
+                                                        <button className='w-[125px] h-[30px] text-[#A03037] border border-[#A03037] !my-[10px] hover:cursor-pointer hover:bg-[#B03A42] hover:text-white duration-300'>
+                                                            Login/Signup
+                                                        </button>
+                                                    </NavLink>
+                                                    <div className='w-[90%] border border-[#878787] !mb-[10px] !mt-[5px]'></div>
+                                                    <div className='flex gap-1 text-[#878787] items-center'>
+                                                        <Wallet className='h-[15px] w-[15px]' />
+                                                        <NavLink to="/wishlist" className="px-3 py-2 hover:bg-gray-100 text-[15px]">My Wishlist</NavLink>
+                                                    </div>
+                                                    <div className='flex gap-1 text-[#878787] items-center'>
+                                                        <Heart className='h-[15px] w-[15px]' />
+                                                        <NavLink to="/myorder" className="px-3 py-2 hover:bg-gray-100 text-[15px]">My Orders</NavLink>
+                                                    </div>
+                                                </>
+                                            })()
+                                        }
+                                    </div>
+                                )
+                            }
+
                         </div>
                         <NavLink to={'/mycart'}>
                             <div className='bg-[#A03037] h-[100%] w-[80px] flex flex-col justify-center items-center gap-1 text-white max-md:w-[75px] max-sm:w-[50px]'>
@@ -40,7 +101,7 @@ function Navbar() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
