90a7a50dcbdddb5d790304d2c66704e584d07351
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-14 11:19:38 +0530

    [Sparsh] Refactor: Navbar Added to Please Login page

--- src/pages/PleaseLoginPage.tsx
+++ src/pages/PleaseLoginPage.tsx
@@ -1,7 +1,10 @@
 import pleaseLoginImage from '../assets/please-login-image.svg';
+import Navbar from '../components/Navbar';
 
 function PleaseLoginPage() {
     return (
+        <>
+        <Navbar />
         <div className="flex justify-center items-center h-[100vh] w-[100%]">
             <div className="w-[275px] flex flex-col items-center justify-center">
                 <p className="text-[25px]">Please Login</p>
@@ -10,6 +13,7 @@ function PleaseLoginPage() {
                 <button className='border border-[#A03037] w-[130px] h-[35px] text-[#A03037] text-[12px] cursor-pointer hover:bg-[#A03037] hover:text-white transition-all duration-300'>Login/Signup</button>
             </div>
         </div>
+        </>
     )
 }
 
