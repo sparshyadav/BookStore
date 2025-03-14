932c659856157f3fc9db41cf0cddb14105a7eee5
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-14 10:35:02 +0530

    [Sparsh] Refactor: Dynamically Rendered Prices/Discounted Price

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -2,11 +2,14 @@ import { Star } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
 
 type data = {
-    cover: string,
+    discountPrice: number;
+    bookName: string;
     title: string,
     author: string,
     rating: number,
     price: number,
+    quantity: number,
+    cover: string;
 }
 
 type bookProps = {
@@ -18,16 +21,22 @@ function BookCard({ data }: bookProps) {
         <NavLink to='/bookpage/1'>
             <div className="h-[275px] w-[235px] border border-[#E2E2E2] rounded-[3px] hover:shadow-2xl transition-all duration-300 hover:border-[#C0C0C0] max-[500px]:w-[175px]">
                 <div className='h-[171px] w-[100%] bg-[#F5F5F5] flex justify-center items-center'>
-                    <img src={data.cover} />
+                    <img src={data.cover} className='h-[135px] w-[105px]' />
                 </div>
                 <div className='h-[102px] w-[100%] !pl-[10px] !pt-[10px] flex flex-col gap-[3px]'>
-                    <p className='text-[14px] font-medium'>{data.title}</p>
+                    <p className='text-[14px] font-medium'>{data.bookName}</p>
                     <p className='text-[grey] text-[10px]'>by {data.author}</p>
-                    <div className='flex items-center justify-center gap-[5px] bg-[#388E3C] rounded-[1px] w-[33px] h-[16px] text-white'>
-                        <p className='text-[10px]'>{data.rating}</p>
-                        <Star className='w-[9px] h-[9px]' fill='white' />
+                    <div className='flex gap-[5px] items-center'>
+                        <div className='flex items-center justify-center gap-[5px] bg-[#388E3C] rounded-[1px] w-[33px] h-[16px] text-white'>
+                            <p className='text-[10px]'>4.5</p>
+                            <Star className='w-[9px] h-[9px]' fill='white' />
+                        </div>
+                        <p className='text-[#878787] text-[10px]'>({data.quantity})</p>
                     </div>
-                    <p className='12px font-medium'>Rs. {data.price}</p>
+                    <div className='flex items-center gap-[5px]'>
+                        <p className='12px font-medium'>Rs. {data.discountPrice}</p>
+                        <p className='line-through text-[#878787] text-[10px] !mt-[4px]'>Rs.{data.price}</p>
+                    </div>
                 </div>
             </div>
         </NavLink>
