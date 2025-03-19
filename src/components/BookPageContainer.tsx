b9fd461c087a75a88edae6cdb0c5d1a22342a507
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-19 18:01:48 +0530

    [Sparsh] Add: Functionality to Add/Remove To/From Wishlist Implemented

--- src/components/BookPageContainer.tsx
+++ src/components/BookPageContainer.tsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 import bookImage2 from '../assets/bookCover2.png'
 import { Star, Dot, Heart } from 'lucide-react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import FeedbackBookPage from './FeedbackBookPage';
+import { toast } from 'react-toastify';
+import { addWishlist, removeWishlist } from '../utils/API';
 
 function BookPageContainer() {
     const [showImage1, setShowImage1] = useState(true);
+    const [isWishlisted, setIsWishlisted]=useState(false);
     const location = useLocation();
     const { data } = location.state || {};
+    const {bookId}=useParams();
+
+    const handleWishlist = async () => {
+        try {
+            if (isWishlisted) {
+                const response = await removeWishlist(bookId);
+                console.log("RESPONSE FROM REMOVE WISHLIST: ", response);
+                toast.error("Item Removed From Wishlist! ❌");
+                setIsWishlisted(false); 
+            } else {
+                const response = await addWishlist(bookId);
+                console.log("RESPONSE FROM ADD WISHLIST: ", response);
+                toast.success("Item Added to Wishlist! ✅");
+                setIsWishlisted(true); 
+            }
+        } catch (error) {
+            console.error("Wishlist operation failed:", error);
+            toast.error("Something went wrong! ❌");
+        }
+    };    
+    
 
     return (
         <div className='!mt-[60px] w-[100%] flex flex-col items-center !my-[35px]'>
@@ -42,12 +66,16 @@ function BookPageContainer() {
                             <img src={showImage1 ? data.cover : bookImage2} className='h-[90%] max-[1050px]:h-[70%]' />
                         </div>
                         <div className="w-[100%] flex flex-col lg:flex-row justify-between gap-4 lg:gap-6 lg:w-full">
-                            <button className="h-[40px] text-white rounded-[2px] w-full lg:w-[49%] bg-[#A03037] hover:bg-[#8C282E] text-sm lg:text-base">
+                            <button className="cursor-pointer h-[40px] text-white rounded-[2px] w-full lg:w-[49%] bg-[#A03037] hover:bg-[#8C282E] text-sm lg:text-base">
                                 ADD TO BAG
                             </button>
-                            <button className="h-[40px] bg-[#333333] rounded-[2px] w-full lg:w-[49%] text-white flex justify-center items-center gap-[5px] hover:bg-[#4D4D4D] text-sm lg:text-base">
+                            <button onClick={handleWishlist} className="cursor-pointer h-[40px] bg-[#333333] rounded-[2px] w-full lg:w-[49%] text-white flex justify-center items-center gap-[5px] hover:bg-[#4D4D4D] text-sm lg:text-base">
                                 <Heart className='w-[18px] h-[18px]' fill='white' />
-                                <p>WISHLIST</p>
+                                {
+                                    isWishlisted?
+                                    <p>WISHLISTED</p>:
+                                    <p>WISHLIST</p>
+                                }
                             </button>
                         </div>
                     </div>
