ec7bc8706fe5e0e2ea864e0ae23ebc1121e1ef73
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-21 23:57:16 +0530

    [Sparsh] Add: Functionality to Increase/Decrease Cart Items Implemented

--- src/components/MyCartContainer.tsx
+++ src/components/MyCartContainer.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Select } from "antd";
 import { MapPin } from 'lucide-react'
-import { getCartItems, removeCartItems } from '../utils/API';
 import BookCover1 from '../assets/BookCover1.png';
 import BookCover2 from '../assets/BookCover2.png';
 import BookCover3 from '../assets/BookCover3.png';
@@ -11,44 +10,21 @@ import BookCover6 from '../assets/BookCover6.png';
 import BookCover7 from '../assets/BookCover7.png';
 import BookCover8 from '../assets/BookCover8.png';
 import BookCover9 from '../assets/BookCover9.png';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCartItems, deleteCartItem, CartItem } from "../redux/cartSlice";
+import type { AppDispatch, RootState } from "../redux/store";
+import { updateCartItems } from '../utils/API';
 import { toast } from 'react-toastify';
 
 const { Option } = Select;
 
-interface Product {
-  bookName: string;
-  author: string;
-  discountPrice: number;
-  _id: string
-}
-
-interface CartItem {
-  product_id: Product;
-  _id: string;
-}
-
 function MyCartContainer() {
   const [selectedAddress, setSelectedAddress] = useState(null);
-  const [count, setCount] = useState<number>(1);
-  const [allCartItems, setAllCartItems] = useState<CartItem[]>([]);
+  const dispatch = useDispatch<AppDispatch>();
 
   const handleChange = (value: React.SetStateAction<null>) => {
     setSelectedAddress(value);
   };
-
-  useEffect(() => {
-    const fetchCartItems = async () => {
-      try {
-        const response = await getCartItems();
-        console.log("RESPONSE: ", response);
-        setAllCartItems(response);
-      } catch (error) {
-        console.error("Error fetching wishlist:", error);
-      }
-    };
-
-    fetchCartItems();
-  }, [])
 
   const bookCovers = [
     BookCover1, BookCover2, BookCover3, BookCover4,
@@ -56,20 +32,55 @@ function MyCartContainer() {
     BookCover9
   ];
 
-  const handleRemove = async (bookId: string) => {
+  const { items, } = useSelector(
+    (state: RootState) => state.cart
+  );
+
+  console.log("ITEMS INSIDE Cart: ", items);
+  useEffect(() => {
+    dispatch(fetchCartItems());
+  }, [dispatch]);
+
+  const handleRemove = (bookId: string) => {
+    dispatch(deleteCartItem(bookId))
+      .then(() => console.log("Item removed successfully"))
+      .catch((err: unknown) => console.error("Failed to remove item", err));
+  };
+
+  const handleIncrease = async (book: CartItem) => {
+    const updatedQuantity = book.quantityToBuy + 1;
+
     try {
-      await removeCartItems(bookId);
-      setAllCartItems(prevItems => prevItems.filter(book => book._id !== bookId));
-      toast.success("Book Removed Successfully! ✅");
-    } catch (error) {
-      console.error("Error removing item from cart:", error);
-      toast.error("Failed to remove book. ❌");
+      const response = await updateCartItems(book._id, updatedQuantity);
+      toast.success("Quantity Increased Successfully! ✅")
+      console.log("INCREASE RESPONSE: ", response);
+      dispatch(fetchCartItems());
+      console.log("Quantity increased successfully");
+    } catch (err) {
+      console.error("Failed to increase quantity", err);
+    }
+  };
+
+  const handleDecrease = async (book: CartItem) => {
+    if (book.quantityToBuy > 1) {
+      const updatedQuantity = book.quantityToBuy - 1;
+
+      try {
+        await updateCartItems(book._id, updatedQuantity);
+        toast.success("Quantity Decreased Successfully! ✅")
+        dispatch(fetchCartItems());
+        console.log("Quantity decreased successfully");
+      } catch (err) {
+        console.error("Failed to decrease quantity", err);
+      }
+    } else {
+      console.log("Quantity cannot be less than 1");
     }
   };
 
 
   return (
-    <div className='min-h-[93vh] flex flex-col items-center px-4'>
+    <div className='!mb-[30px] flex flex-col items-center px-4'>
       <div className='w-[68%] !mt-[60px] max-[1020px]:w-[90%] max-md:w-[90%]'>
         <nav aria-label="breadcrumb" className="w-full max-w-[800px] min-h-[75px] flex items-center max-md:w-[90%]">
           <ol className="flex w-full flex-wrap items-center rounded-md bg-slate-50 px-4 py-2">
@@ -83,7 +94,6 @@ function MyCartContainer() {
             </li>
           </ol>
         </nav>
-
         <div className='w-full h-auto'>
           <div>
             <div className='!mb-[25px] !pl-[15px] border border-[#acabab] min-h-[50px] max-[550px]:!ml-[0px] md:min-h-[250px] w-full max-w-[775px] flex flex-col p-4 max-[550px]:h-auto'>
@@ -109,9 +119,8 @@ function MyCartContainer() {
                   </Select>
                 </div>
               </div>
-
               <div className='w-full flex flex-wrap mt-3 p-3 h-auto'>
-                {allCartItems.map((book, index) => (
+                {items.map((book, index) => (
                   <div key={index} className='!my-[15px] flex w-full gap-[35px] max-[550px]:!mt-[10px] max-[550px]:items-start max-[550px]:justify-start max-[550px]:!px-[10px] !mb-[25px]'>
                     <img src={bookCovers[index % bookCovers.length]} className='!pt-[25px] h-[100px] w-[65px] max-[425px]:h-[90px] max-[425px]:w-[65px] max-[550px]:!mt-[5px]' />
                     <div className='w-full md:w-[75%] flex flex-col gap-[10px] max-[550px]:justify-start '>
@@ -120,16 +129,16 @@ function MyCartContainer() {
                       <p className='text-[15px]'>Rs. {book.product_id.discountPrice}</p>
                       <div className="flex items-center gap-3 mt-3">
                         <button
-                          onClick={() => setCount(count > 1 ? count - 1 : 1)}
+                          onClick={() => handleDecrease(book)}
                           className="w-[25px] h-[25px] flex items-center justify-center rounded-full text-[15px] text-black text-lg font-bold border border-gray-400"
                         >
                           -
                         </button>
                         <div className="w-[40px] h-[25px] flex items-center justify-center text-[15px] border border-gray-400 bg-white text-lg font-semibold">
-                          {count}
+                          {book.quantityToBuy}
                         </div>
                         <button
-                          onClick={() => setCount(count + 1)}
+                          onClick={() => handleIncrease(book)}
                           className="w-[25px] h-[25px] flex items-center justify-center text-[15px] rounded-full text-black text-lg font-bold border border-gray-400"
                         >
                           +
@@ -140,97 +149,6 @@ function MyCartContainer() {
                   </div>
                 ))}
               </div>
-
-              <div>
-                <div className='!pl-[15px] border border-[#acabab] min-h-[250px] max-[550px]:!ml-[0px] md:min-h-[250px] w-full max-w-[775px] flex flex-col p-4 max-[550px]:h-auto'>
-                  <div className='!py-[15px] w-full flex justify-between md:flex-row items-center max-[550px]:!mt-[0px] max-[550px]:gap-[15px] max-[550px]:!px-[10px] cursor-pointer'>
-                    <p className='text-[18px]'>Customer Details</p>
-                    <div className="md:min-w-[155px] max-md:mt-2 max-[550px]:w-[175px] max-[550px]:!mb-[10px] !mr-[30px] max-[550px]:!mr-[0px] ">
-                      <button
-                        className='!px-[25px] !py-[7px] border border-[#A03037] rounded-[3px] text-[#A03037] text-[14px] cursor-pointer'
-                      >
-                        Add New Address
-                      </button>
-                    </div>
-                  </div>
-
-                  <div className='w-[100%] flex flex-col !mb-3 p-3 h-auto gap-y-5'>
-                    <div className='w-[80%] flex flex-col gap-y-5'>
-                      <div className='flex gap-x-5 w-[100%] '>
-                        <div className='flex flex-col justify-left '>
-                          <p className='text-[12px]'>
-                            Full Name
-                          </p>
-                          <input
-                            type="text"
-                            className="!py-[10px] !px-[15px] w-[290px] !h-[50px] border border-[#DCDCDC] rounded-[3px]"
-                            value={"Raghav Khattar"}
-                          />
-                        </div>
-
-                        <div className='flex flex-col justify-left'>
-                          <p className='text-[12px]'>
-                            Mobile Number
-                          </p>
-                          <input
-                            type="text"
-                            className="!py-[10px] !px-[15px] w-[290px] !h-[50px] border border-[#DCDCDC] rounded-[3px]"
-                            value={81678954778}
-                          />
-                        </div>
-                      </div>
-
-                      <div className='flex flex-col gap-y-5'>
-                        <div className='flex gap-x-10 items-center'>
-                          <div className='flex gap-x-2 items-center'>
-                            <img
-                              src='' alt="no image found"
-                              className='w-[15px] h-[15px]' />
-                            <p className='font-semibold'>1.WORK</p>
-                          </div>
-                          <div>
-                            <p className='text-[#A03037] font-semibold cursor-pointer'>Edit</p>
-                          </div>
-                        </div>
-                        <div>
-                          <p>Address</p>
-                          <textarea value={"BridgeLabz Solutions LLP, No. 42, 14th Main, 15th Cross, Sector 4, Opp to BDA complex, near Kumarakom restaurant, HSR Layout, Bangalore"}
-                            className='border border-[#DCDCDC] w-[100%] h-[85px] text-[12px] !p-[10px] text-[#878787] align-left resize-none rounded-[3px] bg-[#F5F5F5]'
-                          />
-                        </div>
-                        <div className='flex gap-x-5'>
-                          <div className='flex flex-col justify-left'>
-                            <p className='text-[12px]'>
-                              City/Town
-                            </p>
-                            <input
-                              type="text"
-                              className="!py-[10px] !px-[15px] w-[290px] !h-[50px] border border-[#DCDCDC] rounded-[3px] bg-[#F5F5F5]"
-                              value={"Bengaluru"}
-                            />
-                          </div>
-                          <div className='flex flex-col justify-left'>
-                            <p className='text-[12px]'>
-                              State
-                            </p>
-                            <input
-                              type="text"
-                              className="!py-[10px] !px-[15px] w-[290px] !h-[50px] border border-[#DCDCDC] rounded-[3px] bg-[#F5F5F5]"
-                              value={"Karnataka"}
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className='flex justify-end w-[98%] !mb-[20px]'>
-                      <button className=' bg-[#3371B5] text-white text-[15px] !px-[30px] !py-[7px] rounded-[3px] cursor-pointer' >
-                        CONTINUE
-                      </button>
-                    </div>
-                  </div>
-
-                </div>
-              </div>
             </div>
           </div>
         </div>
@@ -240,3 +158,8 @@ function MyCartContainer() {
 }
 
 export default MyCartContainer;
+
+
+
+
+
