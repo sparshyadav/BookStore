5e7c23097d3646154d08cbad1c5fa30a4642db3f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-21 23:54:23 +0530

    [Sparsh] Refator: User Address Component, and Order Summary Component Added to MyCart Page

--- src/pages/MyCartPage.tsx
+++ src/pages/MyCartPage.tsx
@@ -1,12 +1,16 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import MyCartContainer from '../components/MyCartContainer'
+import AddressCart from '../components/AddressCart'
+import OrderSummary from '../components/OrderSummary'
 
 function MyCartPage() {
     return (
         <div>
             <Navbar />
             <MyCartContainer />
+            <AddressCart />
+            <OrderSummary />
             <Footer />
         </div>
     )
