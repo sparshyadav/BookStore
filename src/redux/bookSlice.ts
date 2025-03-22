8eddb4aa15b52e8a15cfd4110941be2b27206581
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-22 17:50:06 +0530

    [Sparsh] Add: Description Type Changed to Unknown

--- src/redux/bookSlice.ts
+++ src/redux/bookSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import { getAllBooks } from "../utils/API";
 
 export interface Book {
+    description: unknown;
     _id: string;
     quantity: number;
     rating: number;
