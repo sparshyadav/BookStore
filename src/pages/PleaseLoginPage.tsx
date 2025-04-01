6e45f8a9ab7c6171db6a25aa87e56d6b72406636
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-04-01 14:31:20 +0530

    [Sparsh] Add: Pages Login Updated

--- src/pages/PleaseLoginPage.tsx
+++ src/pages/PleaseLoginPage.tsx
@@ -10,7 +10,7 @@ function PleaseLoginPage() {
                 <div className="w-[275px] flex flex-col items-center justify-center">
                     <p className="text-[25px]">Please Login</p>
                     <p className="text-[#9D9D9D] text-[15px]">Login to view items in your wishlist</p>
-                    <img className="h-[75px] w-[65px] !my-[25px]" src={pleaseLoginImage} />
+                    <img className="h-[75px] w-[65px] !my-[25px]" src={pleaseLoginImage} alt='Please Login' />
                     <NavLink to={'/login'}>
                         <button className='border border-[#A03037] w-[130px] h-[35px] text-[#A03037] text-[12px] cursor-pointer hover:bg-[#A03037] hover:text-white transition-all duration-300'>Login/Signup</button>
                     </NavLink>
